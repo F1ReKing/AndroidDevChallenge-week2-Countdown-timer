d3efe27a6203b7c08956f88cfae47a588bf05788
huangyonghuang <huangyonghuang@jze100.com>
2021-03-05 12:30:42 +0800

    update

--- app/src/main/java/com/f1reking/androiddevchallenge/timer/components/TimerScreen.kt
+++ app/src/main/java/com/f1reking/androiddevchallenge/timer/components/TimerScreen.kt
@@ -1,36 +1,50 @@
 package com.f1reking.androiddevchallenge.timer.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 
 import androidx.compose.ui.unit.dp
 import com.f1reking.androiddevchallenge.timer.ui.theme.purple500
+import com.f1reking.androiddevchallenge.timer.ui.theme.white
 
 @Composable
 fun TimerScreen() {
 
-    AnimatedCircle(
-        Modifier
-            .height(190.dp)
-            .fillMaxWidth()
-            .padding(10.dp)
-        , 362F)
+    Surface(color = purple500,modifier = Modifier.fillMaxHeight()) {
+
+        AnimatedCircle(
+            Modifier
+                .height(190.dp)
+                .fillMaxWidth()
+                .padding(50.dp), 362F, white
+        )
+        
+        Text(text = "10")
+    }
+
+
+
 
 }
 
 private const val DividerLengthInDegrees = 1.8f
 
 @Composable
-fun AnimatedCircle(modifier: Modifier = Modifier, sweep: Float) {
+fun AnimatedCircle(modifier: Modifier = Modifier, sweep: Float, color: Color) {
     val stroke = with(LocalDensity.current) { Stroke(5.dp.toPx()) }
     Canvas(modifier) {
         val innerRadius = (size.minDimension - stroke.width) / 2
@@ -39,7 +53,7 @@ fun AnimatedCircle(modifier: Modifier = Modifier, sweep: Float) {
         val size = Size(innerRadius * 2, innerRadius * 2)
         val startAngle = -90f
         drawArc(
-            color = purple500,
+            color = color,
             startAngle = startAngle + DividerLengthInDegrees / 2,
             sweepAngle = sweep - DividerLengthInDegrees,
             topLeft = topLeft,
@@ -47,7 +61,6 @@ fun AnimatedCircle(modifier: Modifier = Modifier, sweep: Float) {
             useCenter = false,
             style = stroke
         )
-
     }
 }
 
